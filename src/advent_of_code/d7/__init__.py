8595cb7ec817f0faa168674b3944c230a3fa6bb1
Vincent Wong <vinymwong@gmail.com>
2023-12-07 17:43:53 +0000

    day 7 ultimate solution in python

---
+++ src/advent_of_code/d7/__init__.py
@@ -0,0 +1,3 @@
+from .d7 import CamelCardParser, CamelCardsHand
+
+__all__ = ["CamelCardParser", "CamelCardsHand"]
--- src/advent_of_code/d7/d7.py
+++ src/advent_of_code/d7/d7.py
@@ -1,6 +1,5 @@
-# In[0]:
 from enum import IntEnum
-from collections import defaultdict
+from collections import Counter
 
 
 class CamelCardsHand(IntEnum):
@@ -13,100 +12,58 @@ class CamelCardsHand(IntEnum):
     FiveOfAKind = 7
 
 
-def get_hand(cards: str):
-    count_dict = defaultdict(int)
-    for c in cards:
-        count_dict[c] += 1
+class CamelCardParser:
+    def parse(self, raw_input: str):
+        l_cards, l_bets = zip(*(line.split() for line in raw_input.splitlines()))
 
-    was_three = False
-    was_two = False
-    hand = CamelCardsHand.HighCard
-    for c, n in count_dict.items():
-        if c == "J":
-            continue
-        if n == 5:
-            hand = CamelCardsHand.FiveOfAKind
-            break
-        elif n == 4:
-            hand = CamelCardsHand.FourOfAKind
-            break
-        elif (n == 3 and was_two) or (n == 2 and was_three):
-            hand = CamelCardsHand.FullHouse
-            break
-        elif n == 3:
-            hand = CamelCardsHand.ThreeOfAKind
-            was_three = True
-        elif n == 2 and was_two:
-            hand = CamelCardsHand.TwoPair
-            break
-        elif n == 2:
-            hand = CamelCardsHand.OnePair
-            was_two = True
+        hand_type_of_cards = (self.get_hand(cards) for cards in l_cards)
+        cards = list(zip(l_cards, l_bets, hand_type_of_cards))
 
-    n_jokes = count_dict["J"]
-    if n_jokes > 0:
-        if n_jokes == 5:
-            hand = CamelCardsHand.FiveOfAKind
-        elif n_jokes == 4:
-            hand = CamelCardsHand.FiveOfAKind
-        elif n_jokes == 3:  # 2 cards left
-            if hand == CamelCardsHand.HighCard:
-                hand = CamelCardsHand.FourOfAKind
-            elif hand == CamelCardsHand.OnePair:
-                hand = CamelCardsHand.FiveOfAKind
-        elif n_jokes == 2:  # 3 cards left
-            if hand == CamelCardsHand.HighCard:
-                hand = CamelCardsHand.ThreeOfAKind
-            elif hand == CamelCardsHand.OnePair:
-                hand = CamelCardsHand.FourOfAKind
-            elif hand == CamelCardsHand.ThreeOfAKind:
-                hand = CamelCardsHand.FiveOfAKind
-        elif n_jokes == 1:  # 4 cards left
-            if hand == CamelCardsHand.HighCard:
-                hand = CamelCardsHand.OnePair
-            elif hand == CamelCardsHand.OnePair:
-                hand = CamelCardsHand.ThreeOfAKind
-            elif hand == CamelCardsHand.TwoPair:
-                hand = CamelCardsHand.FullHouse
-            elif hand == CamelCardsHand.ThreeOfAKind:
-                hand = CamelCardsHand.FourOfAKind
-            elif hand == CamelCardsHand.FourOfAKind:
-                hand = CamelCardsHand.FiveOfAKind
+        def custom_key(input_tuple: tuple[str, int, CamelCardsHand]):
+            cs, _, c_type = input_tuple
+            c_order = ["J23456789TJQKA".index(c) for c in cs]
+            return (c_type, c_order)
 
-    return hand
+        self.sorted_cards = sorted(cards, key=custom_key)
+
+    @property
+    def total_bets(self):
+        return sum(
+            (
+                (rank_idx + 1) * bet
+                for rank_idx, (_, bet, _) in enumerate(self.sorted_cards)
+            )
+        )
+
+    @staticmethod
+    def get_hand(cards: str):
+        count_dict = Counter(cards)
+        js = count_dict.pop("J") if "J" in count_dict else 0
+        c = Counter(list(count_dict.values()))
+
+        if 5 - js in c or js == 5:
+            return CamelCardsHand.FiveOfAKind
+        elif 4 - js in c:
+            return CamelCardsHand.FourOfAKind
+        elif (c[2] == 2 and js == 1) or (c[3] and c[2]):
+            return CamelCardsHand.FullHouse
+        elif 3 - js in c:
+            return CamelCardsHand.ThreeOfAKind
+        elif c[2] == 2:
+            return CamelCardsHand.TwoPair
+        elif 2 - js in c:
+            return CamelCardsHand.OnePair
+        else:
+            return CamelCardsHand.HighCard
 
 
-# In[1]:
+if __name__ == "__main__":
+    from pathlib import Path
 
-input_file = [
-    (x.split()[0], int(x.split()[1])) for x in open("input.txt").read().splitlines()
-]
+    filename = Path(__file__).resolve().parent / "input.txt"
+    with open(filename) as file_read:
+        raw_input = file_read.read()
 
-print(input_file)
-
-
-# %%
-hands = []
-for cards, bet in input_file:
-    hands.append(get_hand(cards))
-
-# %%
-cards = list(zip([x[0] for x in input_file], [x[1] for x in input_file], hands))
-
-
-def custom_key(input_set: tuple[str, int, CamelCardsHand]):
-    cs, _, c_type = input_set
-    c_order = ["J23456789TJQKA".index(c) for c in cs]
-    return (c_type, c_order)
-
-
-sorted_cards = sorted(cards, key=custom_key)
-
-for card in sorted_cards:
-    print(card)
-# %%
-
-bets = [(i + 1) * b for i, (cs, b, _) in enumerate(sorted_cards)]
-print(bets)
-sum(bets)
-# %%
+    parser = CamelCardParser()
+    parser.parse(raw_input)
+    assert parser.total_bets == 249356515
